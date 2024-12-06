36d4bdef4359b1ce21f9bdcf3f2d03eaea42f912
Jeongseok Lee <jeongseok@meta.com>
2024-12-06 13:54:14 -0800

    Attempt to fix cudatoolkit finding

--- recipe/build_py.sh
+++ recipe/build_py.sh
@@ -2,6 +2,27 @@
 
 set -euxo pipefail
 
+if [[ ${cuda_compiler_version} != "None" ]]; then
+    # PyTorch has multiple different bits of logic finding CUDA, override
+    # all of them.
+    export CUDAToolkit_BIN_DIR=${BUILD_PREFIX}/bin
+    export CUDAToolkit_ROOT_DIR=${PREFIX}
+    if [[ "${target_platform}" != "${build_platform}" ]]; then
+        export CUDA_TOOLKIT_ROOT=${PREFIX}
+    fi
+    case ${target_platform} in
+        linux-64)
+            export CUDAToolkit_TARGET_DIR=${PREFIX}/targets/x86_64-linux
+            ;;
+        linux-aarch64)
+            export CUDAToolkit_TARGET_DIR=${PREFIX}/targets/sbsa-linux
+            ;;
+        *)
+            echo "unknown CUDA arch, edit build.sh"
+            exit 1
+    esac
+fi
+
 # Install the current package with verbose output
 python -m pip install . -vv \
     --global-option=build_ext \
