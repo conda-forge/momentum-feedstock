3f8b496de2127d50dd57a8a60b90663f503f1027
Jeongseok Lee <jeongseok@meta.com>
2024-12-09 15:08:57 -0800

    test

--- recipe/build_py.sh
+++ recipe/build_py.sh
@@ -3,25 +3,32 @@
 set -euxo pipefail
 
 if [[ ${cuda_compiler_version} != "None" ]]; then
-  # Set the CUDA arch list from
-  # https://github.com/conda-forge/pytorch-cpu-feedstock/blob/main/recipe/build_pytorch.sh
-  if [[ ${cuda_compiler_version} == 11.8 ]]; then
-    export TORCH_CUDA_ARCH_LIST="3.5;5.0;6.0;6.1;7.0;7.5;8.0;8.6;8.9+PTX"
-    export CUDA_TOOLKIT_ROOT_DIR=$CUDA_HOME
-  elif [[ ${cuda_compiler_version} == 12.0 || ${cuda_compiler_version} == 12.6 ]]; then
-    export TORCH_CUDA_ARCH_LIST="5.0;6.0;6.1;7.0;7.5;8.0;8.6;8.9;9.0+PTX"
-    # $CUDA_HOME not set in CUDA 12.0. Using $PREFIX
-    export CUDA_TOOLKIT_ROOT_DIR="${PREFIX}"
-    # CUDA_HOME must be set for the build to work in torchaudio
-    export CUDA_HOME="${PREFIX}"
-  else
-    echo "unsupported cuda version. edit build.sh"
-    exit 1
-  fi
-
+  # PyTorch has multiple different bits of logic finding CUDA, override
+  # all of them.
+  export CUDAToolkit_BIN_DIR=${BUILD_PREFIX}/bin
+  export CUDAToolkit_ROOT_DIR=${PREFIX}
   if [[ "${target_platform}" != "${build_platform}" ]]; then
     export CUDA_TOOLKIT_ROOT=${PREFIX}
   fi
+  case ${target_platform} in
+    linux-64)
+      export CUDAToolkit_TARGET_DIR=${PREFIX}/targets/x86_64-linux
+      ;;
+    linux-aarch64)
+      export CUDAToolkit_TARGET_DIR=${PREFIX}/targets/sbsa-linux
+      ;;
+    *)
+      echo "unknown CUDA arch, edit build.sh"
+      exit 1
+  esac
+  case ${cuda_compiler_version} in
+    12.6)
+      export TORCH_CUDA_ARCH_LIST="5.0;6.0;6.1;7.0;7.5;8.0;8.6;8.9;9.0+PTX"
+      ;;
+    *)
+      echo "unsupported cuda version. edit build.sh"
+      exit 1
+  esac
 fi
 
 # Set extra cmake arguments that are not set in https://github.com/facebookincubator/momentum/blob/main/pyproject.toml
