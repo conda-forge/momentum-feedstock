ad4bcac1bceb2388523e9991a34b89f8305f7889
Jeongseok Lee <jeongseok@meta.com>
2024-12-08 16:35:27 -0800

    Update pymomentum build script

--- recipe/build_py.sh
+++ recipe/build_py.sh
@@ -2,20 +2,13 @@
 
 set -euxo pipefail
 
-# Install the current package with verbose output
-python -m pip install . -vv \
-    --global-option=build_ext \
-    --global-option=--cmake-args='
-        -DMOMENTUM_BUILD_IO_FBX=OFF
-        -DMOMENTUM_BUILD_EXAMPLES=OFF
-        -DMOMENTUM_BUILD_TESTING=ON
-        -DMOMENTUM_ENABLE_SIMD=OFF
-        -DMOMENTUM_USE_SYSTEM_GOOGLETEST=ON
-        -DMOMENTUM_USE_SYSTEM_PYBIND11=OFF
-        -DMOMENTUM_USE_SYSTEM_RERUN_CPP_SDK=ON
-    '
+# Set extra cmake arguments that are not set in https://github.com/facebookincubator/momentum/blob/main/pyproject.toml
+export CMAKE_ARGS=" \
+  -DMOMENTUM_ENABLE_SIMD=OFF \
+"
 
-# Copy all .so files to the target directory except those containing 'test' in the filename
-mkdir -p "$SP_DIR/pymomentum"
-find pymomentum -name "*.so" ! -name "*test*" -exec cp {} "$SP_DIR/pymomentum" \;
+# Install the current package with verbose output
+python -m pip install . -vv
+
+# To check if all the necessary package files are installed
 ls "$SP_DIR/pymomentum"
