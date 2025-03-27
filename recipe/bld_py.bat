60445b1f7826be5682eb4b0f25e149546d90558b
Jeongseok Lee <jeongseok@meta.com>
2025-03-27 15:07:33 -0700

    Enable pymomentum + CUDA for Windows
    Increase build number to 1

--- recipe/bld_py.bat
+++ recipe/bld_py.bat
@@ -1,5 +1,19 @@
 @echo on
 
+echo CUDA_HOME is set to: %CUDA_HOME%
+
+echo PATH is set to: %PATH%
+
+echo CMAKE_ARGS before is set to: %CMAKE_ARGS%
+
+@REM set "CMAKE_ARGS=%CMAKE_ARGS:\=\\%"
+
+@REM set "CMAKE_ARGS=%CMAKE_ARGS:;= %"
+
+echo CMAKE_ARGS after is set to: %CMAKE_ARGS%
+
+set CONDA_BUILD_SHORT_BUILD_PREFIX=1
+
 nvcc --version
 
 set CMAKE_ARGS=%CMAKE_ARGS% ^
@@ -7,5 +21,15 @@ set CMAKE_ARGS=%CMAKE_ARGS% ^
     -DMOMENTUM_USE_SYSTEM_GOOGLETEST=ON ^
     -DMOMENTUM_USE_SYSTEM_PYBIND11=ON ^
     -DMOMENTUM_USE_SYSTEM_RERUN_CPP_SDK=ON
+if errorlevel 1 exit 1
 
-%PYTHON% -m pip install . -vv --no-deps --no-build-isolation
+
+if EXIST build (
+    cmake --build build --target clean
+    if %ERRORLEVEL% neq 0 exit 1
+)
+
+@REM %PYTHON% -m pip install --no-deps --ignore-installed . -vv --prefix=%PREFIX%
+%PYTHON% -m pip install --no-deps --ignore-installed . -vv --prefix=%PREFIX%
+if errorlevel 1 exit 1
+
