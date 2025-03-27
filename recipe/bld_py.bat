40b52cef7facea9522675bc9c59be0a2a4a266f4
Jeongseok Lee <jeongseok@meta.com>
2025-03-27 15:07:33 -0700

    Enable pymomentum + CUDA for Windows

--- recipe/bld_py.bat
+++ recipe/bld_py.bat
@@ -2,6 +2,8 @@
 
 where nvcc >nul 2>&1 && nvcc --version
 
+set TORCH_CUDA_ARCH_LIST="5.0;6.0;6.1;7.0;7.5;8.0;8.6;8.9;9.0+PTX"
+
 set CMAKE_ARGS=%CMAKE_ARGS% ^
     -DMOMENTUM_BUILD_RENDERER=OFF ^
     -DMOMENTUM_BUILD_TESTING=OFF ^
@@ -9,5 +11,13 @@ set CMAKE_ARGS=%CMAKE_ARGS% ^
     -DMOMENTUM_USE_SYSTEM_GOOGLETEST=ON ^
     -DMOMENTUM_USE_SYSTEM_PYBIND11=ON ^
     -DMOMENTUM_USE_SYSTEM_RERUN_CPP_SDK=ON
+if errorlevel 1 exit 1
+
+
+if EXIST build (
+    cmake --build build --target clean
+    if %ERRORLEVEL% neq 0 exit 1
+)
 
 %PYTHON% -m pip install . -vv --no-deps --no-build-isolation
+if errorlevel 1 exit 1
