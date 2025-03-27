3fcd53cd74f7a42a29aa4650f1037b5fb2f8054e
Jeongseok Lee <jeongseok@meta.com>
2025-03-27 15:07:33 -0700

    Enable pymomentum + CUDA

--- recipe/bld_py.bat
+++ recipe/bld_py.bat
@@ -1,10 +1,11 @@
 @echo on
 
-nvcc --version
-
 set CMAKE_ARGS=%CMAKE_ARGS% ^
     -DMOMENTUM_USE_SYSTEM_GOOGLETEST=ON ^
     -DMOMENTUM_USE_SYSTEM_PYBIND11=ON ^
     -DMOMENTUM_USE_SYSTEM_RERUN_CPP_SDK=ON
+if errorlevel 1 exit 1
 
-python -m pip install --no-deps --ignore-installed . -vv --prefix=%PREFIX%
+%PYTHON% -m pip install --no-build-isolation --no-deps --ignore-installed .
+@REM %PYTHON% -m pip install --no-deps --ignore-installed . -vv --prefix=%PREFIX%
+if errorlevel 1 exit 1
