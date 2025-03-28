a4adc2aa800b44757ab5db37089d8f32497214cc
Jeongseok Lee <jeongseok@meta.com>
2025-03-28 14:36:50 -0700

    Use SKBUILD_CMAKE_ARGS instead of CMAKE_ARGS

--- recipe/bld_py.bat
+++ recipe/bld_py.bat
@@ -1,11 +1,10 @@
 @echo on
 
-set CMAKE_ARGS=%CMAKE_ARGS% ^
-    -DMOMENTUM_USE_SYSTEM_GOOGLETEST=ON ^
-    -DMOMENTUM_USE_SYSTEM_PYBIND11=ON ^
+set SKBUILD_CMAKE_ARGS=%SKBUILD_CMAKE_ARGS%; ^
+    -DMOMENTUM_USE_SYSTEM_GOOGLETEST=ON; ^
+    -DMOMENTUM_USE_SYSTEM_PYBIND11=ON; ^
     -DMOMENTUM_USE_SYSTEM_RERUN_CPP_SDK=ON
 if errorlevel 1 exit 1
 
-%PYTHON% -m pip install --no-build-isolation --no-deps --ignore-installed .
-@REM %PYTHON% -m pip install --no-deps --ignore-installed . -vv --prefix=%PREFIX%
+%PYTHON% -m pip install --no-deps --ignore-installed . -vv --prefix=%PREFIX%
 if errorlevel 1 exit 1
