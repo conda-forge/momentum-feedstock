21f64eec9656b12e41f41acee767ac66f30193a3
Jeongseok Lee <jeongseok@meta.com>
2025-03-29 01:10:30 -0700

    test2

--- recipe/bld_py.bat
+++ recipe/bld_py.bat
@@ -4,11 +4,17 @@ echo CUDA_HOME is set to: %CUDA_HOME%
 
 echo PATH is set to: %PATH%
 
-@REM set CMAKE_ARGS=%CMAKE_ARGS% ^
-@REM     -DMOMENTUM_USE_SYSTEM_GOOGLETEST=ON ^
-@REM     -DMOMENTUM_USE_SYSTEM_PYBIND11=ON ^
-@REM     -DMOMENTUM_USE_SYSTEM_RERUN_CPP_SDK=ON
-@REM if errorlevel 1 exit 1
+echo CMAKE_ARGS before is set to: %CMAKE_ARGS%
+
+set "CMAKE_ARGS=%CMAKE_ARGS:\=\\%"
+
+echo CMAKE_ARGS after is set to: %CMAKE_ARGS%
+
+set CMAKE_ARGS="%CMAKE_ARGS%" ^
+    -DMOMENTUM_USE_SYSTEM_GOOGLETEST=ON ^
+    -DMOMENTUM_USE_SYSTEM_PYBIND11=ON ^
+    -DMOMENTUM_USE_SYSTEM_RERUN_CPP_SDK=ON
+if errorlevel 1 exit 1
 
 %PYTHON% -m pip install --no-deps --ignore-installed . -vv --prefix=%PREFIX%
 if errorlevel 1 exit 1
