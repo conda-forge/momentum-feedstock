1b7273808979b67bdaba20cd85e8e5829d6d9e7f
Jeongseok (JS) Lee <142548112+jeongseok-meta@users.noreply.github.com>
2025-05-04 20:00:06 -0700

    Update bld_py.bat

--- recipe/bld_py.bat
+++ recipe/bld_py.bat
@@ -1,22 +1,6 @@
 @echo on
 
-echo CUDA_HOME is set to: %CUDA_HOME%
-
-echo PATH is set to: %PATH%
-
-echo CMAKE_ARGS before is set to: %CMAKE_ARGS%
-
-@REM set "CMAKE_ARGS=%CMAKE_ARGS:\=\\%"
-
-@REM set "CMAKE_ARGS=%CMAKE_ARGS:;= %"
-
-echo CMAKE_ARGS after is set to: %CMAKE_ARGS%
-
-set CONDA_BUILD_SHORT_BUILD_PREFIX=1
-
 set TORCH_CUDA_ARCH_LIST="5.0;6.0;6.1;7.0;7.5;8.0;8.6;8.9;9.0+PTX"
-
-nvcc --version
 
 set CMAKE_ARGS=%CMAKE_ARGS% ^
     -DMOMENTUM_ENABLE_SIMD=OFF ^
@@ -31,7 +15,6 @@ if EXIST build (
     if %ERRORLEVEL% neq 0 exit 1
 )
 
-@REM %PYTHON% -m pip install --no-deps --ignore-installed . -vv --prefix=%PREFIX%
-%PYTHON% -m pip install --no-deps --ignore-installed . -vv --prefix=%PREFIX%
+%PYTHON% -m pip install . -vv --no-deps --no-build-isolation
 if errorlevel 1 exit 1
 
