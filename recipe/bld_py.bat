edbafbcfaae77b324e7c89687d23b928fefb0768
Jeongseok Lee <jeongseok@meta.com>
2025-11-27 07:23:23 -0800

    Remove PYBIND11_FINDPYTHON=ON to let pybind11 use FindPython3

--- recipe/bld_py.bat
+++ recipe/bld_py.bat
@@ -26,6 +26,11 @@ set PYTHON_LIB=%PYTHON_LIB:\=/%
 for /f "usebackq tokens=*" %%a in (`%PYTHON% -c "import sys; from pathlib import Path; print(Path(sys.prefix) / 'include')"` ) do set PYTHON_INCLUDE=%%a
 set PYTHON_INCLUDE=%PYTHON_INCLUDE:\=/%
 
+echo PYTHON_PREFIX: %PYTHON_PREFIX%
+echo PYTHON_VER: %PYTHON_VER%
+echo PYTHON_LIB: %PYTHON_LIB%
+echo PYTHON_INCLUDE: %PYTHON_INCLUDE%
+
 set CMAKE_ARGS=%CMAKE_ARGS% ^
     -DMOMENTUM_BUILD_IO_USD=OFF ^
     -DMOMENTUM_BUILD_RENDERER=OFF ^
@@ -35,19 +40,12 @@ set CMAKE_ARGS=%CMAKE_ARGS% ^
     -DMOMENTUM_USE_SYSTEM_PYBIND11=ON ^
     -DMOMENTUM_USE_SYSTEM_RERUN_CPP_SDK=ON ^
     -DCMAKE_POLICY_DEFAULT_CMP0148=NEW ^
-    -DPYBIND11_FINDPYTHON=ON ^
     -DPYBIND11_PYTHON_VERSION="%PYTHON_VER%" ^
-    -DPython_ROOT_DIR="%PYTHON_PREFIX%" ^
     -DPython3_ROOT_DIR="%PYTHON_PREFIX%" ^
-    -DPython_EXECUTABLE="%PYTHON%" ^
     -DPython3_EXECUTABLE="%PYTHON%" ^
-    -DPython_LIBRARY="%PYTHON_LIB%" ^
     -DPython3_LIBRARY="%PYTHON_LIB%" ^
-    -DPython_INCLUDE_DIR="%PYTHON_INCLUDE%" ^
     -DPython3_INCLUDE_DIR="%PYTHON_INCLUDE%" ^
-    -DPython_FIND_STRATEGY=LOCATION ^
     -DPython3_FIND_STRATEGY=LOCATION ^
-    -DPython_FIND_REGISTRY=NEVER ^
     -DPython3_FIND_REGISTRY=NEVER
 
 %PYTHON% -m pip install . -vv --no-deps --no-build-isolation
