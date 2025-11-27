d871f28ad1d7b5f68828f660d674d48a9170254c
Jeongseok Lee <jeongseok@meta.com>
2025-11-27 04:47:19 -0800

    Not using ninja

--- recipe/bld_cpp.bat
+++ recipe/bld_cpp.bat
@@ -1,15 +1,8 @@
 @echo on
-setlocal EnableExtensions
-
-rem Prefer Ninja & parallel builds on Windows
-set "CMAKE_GENERATOR=Ninja"
-set "CMAKE_BUILD_PARALLEL_LEVEL=%CPU_COUNT%"
 
 cmake %SRC_DIR% ^
   %CMAKE_ARGS% ^
-  -G Ninja ^
   -B build ^
-  -DCMAKE_BUILD_TYPE=Release ^
   -DBUILD_SHARED_LIBS=ON ^
   -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
   -DCMAKE_PREFIX_PATH=%LIBRARY_PREFIX% ^
@@ -24,11 +17,11 @@ cmake %SRC_DIR% ^
   -DMOMENTUM_USE_SYSTEM_RERUN_CPP_SDK=ON
 if errorlevel 1 exit 1
 
-cmake --build build --parallel
+cmake --build build --parallel --config Release
 if errorlevel 1 exit 1
 
-cmake --install build
+cmake --build build --parallel --config Release --target install
 if errorlevel 1 exit 1
 
-ctest --test-dir build --output-on-failure
+ctest --test-dir build --output-on-failure --build-config Release
 if errorlevel 1 exit 1
